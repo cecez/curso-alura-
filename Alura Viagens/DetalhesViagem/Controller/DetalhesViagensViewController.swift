931adacad7868c1023e4327dfcce8e051c53f301
Cezar Rosa <cecez86@gmail.com>
2020-12-02 18:38:04 -0300

    * customizando os teclados dos textfields (number pad, date, senha e default)

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -16,22 +16,32 @@ class DetalhesViagensViewController: UIViewController {
     @IBOutlet weak var labelDataViagem: UILabel!
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
     @IBOutlet weak var scrollPrincipal: UIScrollView!
+    @IBOutlet weak var textFieldData: UITextField!
+    
+    // MARK: Outras propriedades
+    var pacoteSelecionado: PacoteViagem? = nil
     
     // MARK: IBActions
     @IBAction func botaoVoltar(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    // MARK: Outros
-    var pacoteSelecionado: PacoteViagem? = nil
-    
+    @IBAction func textFieldEntrouFoco(_ sender: UITextField) {
+        // cria componente datePicker e seu formato
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        
+        // exibe componente
+        sender.inputView = datePicker
+        
+        // configura target para componente
+        datePicker.addTarget(self, action: #selector(exibeDataTextField(sender:)), for: .valueChanged)
+    }
     
     // MARK: LC
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("carregou");
-        
+                
         // registro de observer quando teclado surgir
         NotificationCenter.default.addObserver(self, selector: #selector(aumentarScroll(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
 
@@ -44,8 +54,16 @@ class DetalhesViagensViewController: UIViewController {
         }
     }
     
+    // MARK: Outros métodos
     @objc func aumentarScroll(notification: Notification) {
         self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
+    }
+    
+    @objc func exibeDataTextField(sender: UIDatePicker) {
+        let formatador = DateFormatter()
+        formatador.dateFormat = "MM / yyyy"
+        
+        self.textFieldData.text = formatador.string(from: sender.date)
     }
     
     
